d4b72dfd5e85b90b4d5adc10c22fa09278333fb9
Ashraf Manna <optimisticashraf@gmail.com>
2025-07-14 14:56:12 +0600

    API restructed

--- src/app/api/addComment/route.js
+++ src/app/api/addComment/route.js
@@ -1,11 +1,12 @@
-import { corsHeaders } from "@/app/lib/corsHeader/corsHeader";
-import { collectionNameObj, dbConnect } from "@/app/lib/dbConect/dbConect";
+
+import { corsHeaders } from "@/components/lib/corsHeader";
+import dbConnect, { collectionNames } from "@/components/lib/dbConnect";
 import { NextResponse } from "next/server";
 
 
 export async function POST(req, { params }) {
     const body = await req.json()
-    const commentCollection = await dbConnect(collectionNameObj.commentCollection)
+    const commentCollection = await dbConnect(collectionNames.commentCollection)
     const result = await commentCollection.insertOne(body)
     console.log('add comment', result);
     return NextResponse.json(result, {
